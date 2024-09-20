e17300742cc9bf7e66c52c0d85864ea11071c568
Mujahed032 <mohdmujahed032@gmail.com>
2024-09-20 14:41:50 +0530

    Added Migration
    Added Migration

--- Procedures/Procedures/Program.cs
+++ Procedures/Procedures/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Repository.Data;
+
 namespace Procedures
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Procedures
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ApplicationDbContext>( options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly("Procedures")));
 
             var app = builder.Build();
 
---
+++ Procedures/Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<FileUpload> Files { get; set; }
+    }
+}
