e17ec7828f4a4e0501887da250765edb75553813
Mujahed032 <mohdmujahed032@gmail.com>
2024-09-20 14:23:39 +0530

    Added the class Libraries
    Added the class Libraries

---
+++ Procedures/CommonX/IResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonX
+{
+    public interface IResponseModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public int StatusCode { get; set; }
+    }
+
+    public interface IResponseDataModel<T> : IResponseModel
+    {
+        public List<T> Data { get; set; }
+    }
+}
---
+++ Procedures/CommonX/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonX
+{
+    public class ResponseModel : IResponseModel
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public int StatusCode { get; set; }
+    }
+
+    public class ResponseDataModel<T> : ResponseModel, IResponseDataModel<T>
+    {
+        public List<T> Data { get; set; }
+    }
+}
